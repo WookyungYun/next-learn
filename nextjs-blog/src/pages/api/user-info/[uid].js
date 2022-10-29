1ef0298694c1dbf95cb3948b79a5689b59fac1f8
WookyungYun <gyong1540@gmail.com>
2022-10-29 15:20:55 +0900

    API Routes

---
+++ nextjs-blog/src/pages/api/user-info/[uid].js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  const { uid } = req.query
+  const cookies = req.cookies
+  // res.status(200).json({ name: `Jimmy Choi ${uid} ${JSON.stringify(cookies)}` })
+  res.status(500).send({ error: 'error' })
+}
